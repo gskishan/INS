ddeb2c7aa5a5d9499ddbf832734890e43b2927a8
Mohammed  Qutubuddin Zafar <mohammedqutubuddinzafar@Mohammeds-MacBook-Air.local>
2024-07-29 10:20:55 +0530

    Removed String Filter to normal filter

--- ins/ins/report/sales_report/sales_report.js
+++ ins/ins/report/sales_report/sales_report.js
@@ -2,24 +2,24 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Sales Report"] = {
-	"filters": [
+	filters: [
 		{
-			"fieldname":"customer",
-			"label": __("Customer"),
-			"fieldtype": "Link",
-			"options": "Customer",
+			fieldname:"customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
 		},
 		{
-			"fieldname":"from_date",
-			"label": __("Start Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.add_months(frappe.datetime.nowdate(), -1),
+			fieldname:"from_date",
+			label: __("Start Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.nowdate(), -1),
 		},
 		{
-			"fieldname":"to_date",
-			"label": __("End Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.nowdate(),
+			fieldname:"to_date",
+			label: __("End Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.nowdate(),
 		}
 	]
 };
