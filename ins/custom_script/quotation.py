4435f3d8ec58dede8628f6380aa0f8fad0e04211
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-07-30 15:59:46 +0530

    Update quotation.py

--- ins/custom_script/quotation.py
+++ ins/custom_script/quotation.py
@@ -6,16 +6,14 @@ from frappe import _
 
 @frappe.whitelist()
 def before_submit(self,method):
-	# if self.enquiry:
-	# 	if frappe.db.exists("Enquiry", self.enquiry):
-	# 		enq=frappe.get_doc("Enquiry", self.enquiry)
-	# 		enq.db_set("status","Quotation")
-	pass
+	if self.enquiry:
+		if frappe.db.exists("Enquiry", self.enquiry):
+			enq=frappe.get_doc("Enquiry", self.enquiry)
+			enq.db_set("status","Quotation")
 
 @frappe.whitelist()
 def on_cancel(self,method):
-	# if self.enquiry:
-	# 	if frappe.db.exists("Enquiry", self.enquiry):
-	# 		enq=frappe.get_doc("Enquiry", self.enquiry)
-	# 		enq.db_set("status","")
-	pass
+	if self.enquiry:
+		if frappe.db.exists("Enquiry", self.enquiry):
+			enq=frappe.get_doc("Enquiry", self.enquiry)
+			enq.db_set("status","")
