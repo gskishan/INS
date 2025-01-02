bc89f6d853768e97dafe94889a7ec75b1d173129
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-01-02 13:20:07 +0530

    Update sales_invoice.py

--- ins/custom_script/sales_invoice.py
+++ ins/custom_script/sales_invoice.py
@@ -1,6 +1,7 @@
 from frappe import utils
 import re
 import frappe
+from erpnext.accounts.utils import get_fiscal_year
 
 @frappe.whitelist()
 def update_oldrecord():
@@ -27,21 +28,25 @@ def get_year(posting_date):
 def validate(self, method=None):
     if self.is_new() and 'param' in self.naming_series:
         try:
-            sql = """SELECT MAX(sequence) sequence
-                    FROM `tabSales Invoice`
-                    WHERE company=%s AND sequence IS NOT NULL and  YEAR(creation) =%s
-                    ORDER BY creation DESC
-                    LIMIT 1""".format(self.doctype)
-            last_count = frappe.db.sql(sql, (self.company,get_year(self.posting_date)), as_dict=False)
+            fiscal_year_data = get_fiscal_year(date=self.posting_date)
+            fiscal_start = fiscal_year_data[1]  
+            fiscal_end = fiscal_year_data[2] 
+            
+            sql = """
+                SELECT MAX(sequence) 
+                FROM `tabSales Invoice`
+                WHERE company=%s AND sequence IS NOT NULL 
+                AND creation BETWEEN %s AND %s
+            """
+            last_count = frappe.db.sql(sql, (self.company, fiscal_start, fiscal_end), as_dict=False)
+
             last_count = last_count[0][0] if last_count else None
-            if last_count is not None:
-                self.sequence = last_count + 1
-            else:
-                self.sequence = 1	
+            self.sequence = last_count + 1 if last_count else 1	
         except Exception as e:
-            frappe.log_error(f"Error setting  sequence: {str(e)}")
-    if self.is_new() and 'param' not in self.naming_series:
+            frappe.log_error(f"Error setting sequence: {str(e)}")
+    elif self.is_new():
         self.sequence = 0
+
 
     
 
@@ -50,29 +55,33 @@ def autoname(doc, method=None):
 
 def set_name(doc):
     if 'param' in doc.naming_series:
-        from erpnext.accounts.utils import get_fiscal_year
-        year=get_fiscal_year().name
+        fiscal_year_data = get_fiscal_year(date=doc.posting_date)
+        fiscal_start = fiscal_year_data[1]  
+        fiscal_end = fiscal_year_data[2]
+
         month = re.findall(r'\d+', utils.today())[1]
         vertical = doc.vertical
-        type = doc.type
-        company_code = ""
-        if doc.company == "insmart Systems":
-            company_code = "10000"
-        elif doc.company == "insmart Systems India Private Limited":
-            company_code = "20000"
-        elif doc.company == "OIA TECHNOLOGIES PRIVATE LIMITED":
-            company_code = '30000'
-        sql = """SELECT MAX(sequence)
-                    FROM `tab{0}`
-                    WHERE company=%s AND sequence IS NOT NULL  and  YEAR(creation) =%s""".format(doc.doctype)
-        max_icv = frappe.db.sql(sql, (doc.company,get_year(self.posting_date)), as_dict=False)[0][0]
-        if max_icv is not None:
-            sequence = max_icv + 1
-        else:
-            sequence = 1
-        last_number=int(company_code) + sequence
-    
-        if last_number == 0:
-            last_number = int(company_code) + 1
+        doc_type = doc.type
+        
+        company_codes = {
+            "insmart Systems": "10000",
+            "insmart Systems India Private Limited": "20000",
+            "OIA TECHNOLOGIES PRIVATE LIMITED": "30000"
+        }
+        company_code = company_codes.get(doc.company, "00000")
 
-        doc.name = year + type + month + vertical + str(last_number)
+        sql = """
+            SELECT MAX(sequence)
+            FROM `tab{0}`
+            WHERE company=%s AND sequence IS NOT NULL 
+            AND creation BETWEEN %s AND %s
+        """.format(doc.doctype)
+        
+        max_sequence = frappe.db.sql(sql, (doc.company, fiscal_start, fiscal_end), as_dict=False)
+        max_sequence = max_sequence[0][0] if max_sequence else None
+        sequence = max_sequence + 1 if max_sequence else 1
+
+        last_number = int(company_code) + sequence
+        fiscal_year = fiscal_year_data[0].replace('-', '')
+
+        doc.name = f"{fiscal_year}{doc_type}{month}{vertical}{last_number}"
